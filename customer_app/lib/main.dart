6e3e33a61b949cdfc3cbfe2fe77a8f1ba7a2d472
Sudharshan Geddam <darshans1013@gmail.com>
2026-02-20 21:02:45 +0530

    chore: added firebase dependencies to customerapp

--- customer_app/lib/main.dart
+++ customer_app/lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:customer_app/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseFirestore.instance.useFirestoreEmulator('10.0.2.2', 8080);
+  FirebaseAuth.instance.useAuthEmulator('10.0.2.2', 9099);
+  FirebaseFunctions.instance.useFunctionsEmulator('10.0.2.2', 5001);
+  
   runApp(const MyApp());
 }
 
