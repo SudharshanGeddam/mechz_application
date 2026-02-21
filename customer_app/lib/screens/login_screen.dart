dbb0a49fca91bc772703db41518866b11938f7db
Sudharshan Geddam <darshans1013@gmail.com>
2026-02-21 14:34:48 +0530

    ui: updated login screen

--- customer_app/lib/screens/login_screen.dart
+++ customer_app/lib/screens/login_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:customer_app/features/auth/cubit/auth_cubit.dart';
 import 'package:customer_app/features/auth/cubit/auth_state.dart';
 import 'package:customer_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -22,12 +25,12 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void _sendOtp() {
-  if (_formKey.currentState!.validate()) {
-    final formattedNumber = "+91${_phoneController.text.trim()}";
+    if (_formKey.currentState!.validate()) {
+      final formattedNumber = "+91${_phoneController.text.trim()}";
 
-    context.read<AuthCubit>().sendOtp(formattedNumber);
+      context.read<AuthCubit>().sendOtp(formattedNumber);
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -52,84 +55,96 @@ class _LoginScreenState extends State<LoginScreen> {
         },
         child: SafeArea(
           child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height * 0.35,
-                      child: Image.asset(
-                        'assets/images/login_bg.png',
-                        fit: BoxFit.cover,
-                      ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    child: Image.asset(
+                      'assets/images/login_bg.png',
+                      fit: BoxFit.fitWidth,
                     ),
-                    const SizedBox(height: 30),
-                    const Text(
-                      "Let's Get Started",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      "Sign in to continue with your mobile number",
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    const SizedBox(height: 30),
-                    TextFormField(
-                      controller: _phoneController,
-                      keyboardType: TextInputType.number,
-                      maxLength: 10,
-                      decoration: const InputDecoration(
-                        labelText: "Mobile Number",
-                        border: OutlineInputBorder(),
-                        prefixText: "+91 ",
-                        counterText: "",
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Please enter mobile number";
-                        }
-                        if (value.length != 10) {
-                          return "Enter valid 10-digit number";
-                        }
-                        if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
-                          return "Only digits allowed";
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      "An 6-digit OTP will be sent via SMS to verify your mobile number",
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                    const SizedBox(height: 30),
-                    BlocBuilder<AuthCubit, AuthState>(
-                      builder: (context, state) {
-                        final isLoading = state is AuthLoading;
-
-                        return SizedBox(
-                          width: double.infinity,
-                          height: 50,
-                          child: ElevatedButton(
-                            onPressed: isLoading ? null : _sendOtp,
-                            child: isLoading
-                                ? const CircularProgressIndicator(
-                                    color: Colors.white,
-                                  )
-                                : const Text("Send OTP"),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: const Text(
+                            "Let's Get Started",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        );
-                      },
+                        ),
+                        const SizedBox(height: 30),
+                        const Text(
+                          "Sign in to continue with your mobile number",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          controller: _phoneController,
+                          keyboardType: TextInputType.number,
+                          maxLength: 10,
+                          decoration: const InputDecoration(
+                            labelText: "Mobile Number",
+                            border: OutlineInputBorder(),
+                            prefixText: "+91 ",
+                            counterText: "",
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please enter mobile number";
+                            }
+                            if (value.length != 10) {
+                              return "Enter valid 10-digit number";
+                            }
+                            if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+                              return "Only digits allowed";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          "An 6-digit OTP will be sent via SMS to verify your mobile number",
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 30),
+                        BlocBuilder<AuthCubit, AuthState>(
+                          builder: (context, state) {
+                            final isLoading = state is AuthLoading;
+                            return SizedBox(
+                              width: double.infinity,
+                              height: 50,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.orange,
+                                ),
+                                onPressed: isLoading ? null : _sendOtp,
+                                child: isLoading
+                                    ? const CircularProgressIndicator(
+                                        color: Colors.orange,
+                                      )
+                                    : const Text(
+                                        "Send OTP",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -149,22 +164,94 @@ class OtpScreen extends StatefulWidget {
 }
 
 class _OtpScreenState extends State<OtpScreen> {
-  final TextEditingController _otpController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+  final List<TextEditingController> _controllers = List.generate(
+    6,
+    (_) => TextEditingController(),
+  );
+
+  final List<FocusNode> _focusNodes = List.generate(6, (_) => FocusNode());
+
+  int _seconds = 30;
+  Timer? _timer;
 
   @override
-  void dispose() {
-    _otpController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  void _startTimer() {
+    _seconds = 30;
+    _timer?.cancel();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_seconds == 0) {
+        timer.cancel();
+      } else {
+        setState(() => _seconds--);
+      }
+    });
   }
 
   void _verifyOtp() {
-    if (_formKey.currentState!.validate()) {
-      context.read<AuthCubit>().verifyOtp(
-        widget.verificationId,
-        _otpController.text.trim(),
-      );
+    final otp = _controllers.map((e) => e.text).join();
+
+    if (otp.length == 6) {
+      context.read<AuthCubit>().verifyOtp(widget.verificationId, otp);
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Enter valid 6-digit OTP")));
     }
+  }
+
+  void _resendOtp() {
+    _startTimer();
+    // If you have resend method:
+    // context.read<AuthCubit>().resendOtp();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    for (var c in _controllers) {
+      c.dispose();
+    }
+    for (var f in _focusNodes) {
+      f.dispose();
+    }
+    super.dispose();
+  }
+
+  Widget _buildOtpBox(int index) {
+    return SizedBox(
+      width: 45,
+      child: TextField(
+        controller: _controllers[index],
+        focusNode: _focusNodes[index],
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        maxLength: 1,
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        decoration: const InputDecoration(
+          counterText: "",
+          border: OutlineInputBorder(),
+        ),
+        onChanged: (value) {
+          if (value.isNotEmpty && index < 5) {
+            _focusNodes[index + 1].requestFocus();
+          }
+          if (value.isEmpty && index > 0) {
+            _focusNodes[index - 1].requestFocus();
+          }
+
+          // Optional: Auto submit when last digit entered
+          if (index == 5 && value.isNotEmpty) {
+            _verifyOtp();
+          }
+        },
+      ),
+    );
   }
 
   @override
@@ -176,10 +263,7 @@ class _OtpScreenState extends State<OtpScreen> {
           if (state is Authenticated) {
             Navigator.pushAndRemoveUntil(
               context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    const HomeScreen(), 
-              ),
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
               (route) => false,
             );
           }
@@ -191,49 +275,50 @@ class _OtpScreenState extends State<OtpScreen> {
           }
         },
         child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _otpController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: "Enter OTP",
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Enter OTP";
-                    }
-                    if (value.length != 6) {
-                      return "Enter valid 6-digit OTP";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 30),
-                BlocBuilder<AuthCubit, AuthState>(
-                  builder: (context, state) {
-                    final isLoading = state is AuthLoading;
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              const Text("Enter 6-digit OTP", style: TextStyle(fontSize: 18)),
+              const SizedBox(height: 30),
 
-                    return SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: isLoading ? null : _verifyOtp,
-                        child: isLoading
-                            ? const CircularProgressIndicator(
-                                color: Colors.white,
-                              )
-                            : const Text("Verify"),
-                      ),
-                    );
-                  },
+              /// OTP Boxes
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(6, (index) => _buildOtpBox(index)),
+              ),
+
+              const SizedBox(height: 40),
+
+              /// Verify Button
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                  ),
+                  onPressed: _verifyOtp,
+                  child: const Text(
+                    "Verify",
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-              ],
-            ),
+              ),
+
+              const SizedBox(height: 20),
+
+              /// Resend Timer
+              _seconds == 0
+                  ? TextButton(
+                      onPressed: _resendOtp,
+                      child: const Text("Resend OTP"),
+                    )
+                  : Text(
+                      "Resend OTP in $_seconds seconds",
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+            ],
           ),
         ),
       ),
