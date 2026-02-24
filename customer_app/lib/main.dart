7f89c0907c175b74564e6e9b0441dffacacfb5ec
Sudharshan Geddam <darshans1013@gmail.com>
2026-02-24 11:37:52 +0530

    UI: Initial Demo

--- customer_app/lib/main.dart
+++ customer_app/lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
-import 'package:customer_app/features/cubit/auth_cubit.dart';
-import 'package:customer_app/features/cubit/auth_state.dart';
-import 'package:customer_app/features/cubit/service_cubit.dart';
-import 'package:customer_app/features/repository/auth_repository.dart';
-import 'package:customer_app/features/repository/service_repository.dart';
 import 'package:customer_app/firebase_options.dart';
-import 'package:customer_app/screens/home_screen.dart';
-import 'package:customer_app/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,46 +22,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiRepositoryProvider(
-      providers: [
-        RepositoryProvider<AuthRepository>(create: (_) => AuthRepository()),
-        RepositoryProvider<ServiceRepository>(
-          create: (_) => ServiceRepository(),
-        ),
-      ],
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider<AuthCubit>(
-            create: (context) =>
-                AuthCubit(context.read<AuthRepository>())..checkAuthStatus(),
-          ),
-          BlocProvider<ServiceCubit>(
-            create: (context) =>
-                ServiceCubit(context.read<ServiceRepository>())..loadServices(),
-          ),
-        ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: const RootRouter(),
-        ),
+    return Scaffold(
+      body: Center(
+        child: Text("Hello World!"),
       ),
     );
   }
 }
 
-class RootRouter extends StatelessWidget {
-  const RootRouter({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<AuthCubit, AuthState>(
-      builder: (context, state) {
-        if (state is Authenticated) {
-          return const HomeScreen();
-        } else {
-          return const LoginScreen();
-        }
-      },
-    );
-  }
-}
